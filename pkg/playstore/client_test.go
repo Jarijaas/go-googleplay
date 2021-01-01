15ff2f9bb017c80d2a1cc172784f4e0447f07440
Jari Jääskelä <jari.jaaskela@protonmail.com>
2021-01-01 14:12:47 +0200

    TestGetAppDetails, remove print statements

--- pkg/playstore/client_test.go
+++ pkg/playstore/client_test.go
@@ -2,7 +2,6 @@ package playstore
 
 import (
 	"github.com/jarijaas/go-gplayapi/pkg/auth"
-	log "github.com/sirupsen/logrus"
 	"os"
 	"testing"
 )
@@ -38,18 +37,9 @@ func TestGetAppDetails(t *testing.T) {
 		t.Fatalf("Could not get package details: %v", err)
 	}
 
-	log.Infof("Got details: %v", res)
-
 	if *res.Docid != TestPackageName {
 		t.Fatalf("Package name is incorrect: %s, should be: %s", *res.Docid, TestPackageName)
 	}
-
-	/**
-	appDetails = self.details(packageName).get('details').get('appDetails')
-	            versionCode = appDetails.get('versionCode')
-	 */
-
-	log.Infof("App details: %v", res.Details.AppDetails)
 
 	if res.Details.AppDetails.VersionCode == nil {
 		t.Fatalf("Version code is not in the response, is your gsfId correct?")
